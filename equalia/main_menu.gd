c504ab1a002e6d212d0f85cb6c42fdff452e7f3f
Jaden M. <149161366+jedinsama@users.noreply.github.com>
2025-09-21 12:34:13 +0800

    Main Menu

---
+++ equalia/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_newgame_pressed() -> void:
+	print("New Game Created") # Replace with function body.
+
+
+func _on_loadgame_pressed() -> void:
+	print("Loaded Previous Game") # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	print("Options Loaded") # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit() # Replace with function body.
